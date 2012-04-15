870d3a2177ae0e3a2877b8f504af749c91cdac29
Bronsa <brobronsa@gmail.com>
2012-04-15 17:09:06 +0200

    bump clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject brochure "0.0.1"
   :description "Clojure implementation in clojure"
-  :dependencies [[bronsa/clojure "1.4.0-beta7"]]
+  :dependencies [[bronsa/clojure "1.4.0"]]
   :aot [brochure.core])
