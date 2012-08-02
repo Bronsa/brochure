9477b3a508cf55c6e168b06ff4dac03505256401
Bronsa <brobronsa@gmail.com>
2012-08-02 18:42:17 +0200

    switched to https://github.com/Bronsa/clojure/tree/brochure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject brochure "0.0.1"
   :description "Clojure implementation in clojure"
-  :dependencies [[org.clojure/clojure "1.5.0-alpha3"]
+  :dependencies [[brochure/clojure "1.5.0-alpha3"]
                  [org.ow2.asm/asm "4.0"]
                  [org.ow2.asm/asm-commons "4.0"]
                  [org.ow2.asm/asm-util "4.0"]
