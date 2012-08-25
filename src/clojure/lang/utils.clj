9a37c1a6c9bc7c3f0e8db39f6521cb4f050a2396
Bronsa <brobronsa@gmail.com>
2012-08-25 14:46:59 +0200

    added a bunch of functions

--- src/clojure/lang/utils.clj
+++ src/clojure/lang/utils.clj
@@ -1,7 +1,8 @@
 (set! *warn-on-reflection* true)
 
 (ns clojure.lang.utils
-  (:refer-clojure :exclude [hash-combine])
+  (:refer-clojure :exclude [hash-combine hash count to-array])
+  (:require [clojure.lang.protocols :refer :all])
   (:import java.util.Map$Entry
            java.util.concurrent.ConcurrentHashMap
            (java.lang.ref Reference ReferenceQueue)))
@@ -10,14 +11,28 @@
   (if (nil? o) 0
       (.hashCode o)))
 
+(defn hash [o]
+  (if (satisfies? IHash o)
+    (-hash o)
+    (hash-code o)))
+
 (set! *unchecked-math* true)
 (defn hash-combine [seed hash]
   (let [seed (int seed)
         hash (int hash)]
     (bit-xor seed
-             (+ hash 0x9e3779b9
-                (bit-shift-left seed 6) ;; should probably be using Numbers/shiftLeftInt
-                (bit-shift-right seed 2)))))
+             (unchecked-add-int hash
+               (unchecked-add-int 0x9e3779b9
+                 (unchecked-add-int
+                  (clojure.lang.Numbers/shiftLeftInt seed 6)
+                  (clojure.lang.Numbers/shiftRightInt seed 2)))))))
+
+(def seqable-hash-code
+  (memoize
+   (fn [obj]
+     (int (reduce #(unchecked-add-int (int (* 31 %))
+                                      (int (hash %2))) 1 obj)))))
+(set! *unchecked-math* false)
 
 (defn clear-cache [^ReferenceQueue rq ^ConcurrentHashMap cache]
   (when-not (nil? (.poll rq))
@@ -27,3 +42,108 @@
         (if (and (not (nil? val ))
                  (nil? (.get val)))
           (.remove cache (.getKey e) val))))))
+
+(defn equals? [a b]
+  (or (identical? a b)
+      (and (not (nil? a))
+           (.equals ^Object a b))))
+
+(defn equiv? [a b]
+  (or (identical? a b)
+      (and a
+           (if (and (instance? java.lang.Number a)
+                    (instance? java.lang.Number b))
+             (clojure.lang.Numbers/equal a b)
+             (if (satisfies? IPersistentCollection a)
+               (-equiv a b)
+               (if (satisfies? IPersistentCollection b)
+                 (-equiv b a)
+                 (.equals ^Object a b)))))))
+
+
+(deftype Box [val])
+
+(defprotocol IState
+  (-val [this])
+  (-val! [this v])
+  (-rest [this])
+  (-rest! [this v]))
+
+(deftype State [^:volatile-mutable val ^:volatile-mutable rest]
+  IState
+  (-val [_] val)
+  (-val! [_ new-val] (set! val new-val))
+  (-rest [_] rest)
+  (-rest! [_ new-rest] (set! rest new-rest)))
+
+
+(defn count [coll]
+  (cond
+    (nil? coll) 0
+
+    (satisfies? ICounted coll) (-count coll)
+
+    (satisfies? IPersistentCollection coll)
+    (loop [s (-seq coll) c 0]
+      (if (-next s)
+        (if (satisfies? ICounted s)
+          (+ c (-count s))
+          (recur (-next s) (inc c)))
+        c))
+      
+    (instance? CharSequence coll)
+    (.length ^CharSequence coll)
+
+    (instance? java.util.Collection coll)
+    (.size ^java.util.Collection coll)
+      
+    (instance? java.util.Map coll)
+    (.size ^java.util.Map coll)
+      
+    (-> coll .getClass .isArray)
+    (alength coll)
+    
+    :else
+    (throw (UnsupportedOperationException.
+            (str "count not supported on this type: "
+                 (-> coll .getClass .getSimpleName))))))
+
+
+(defn reify-seq ^java.util.List [seq]
+  (java.util.Collections/unmodifiableList (java.util.ArrayList. seq)))
+
+(defn unsigned-bit-shift-right
+  "Shifts the input `x` to the right by `n` places and sets the leftmost bit to 0."
+  [^long x ^long n]
+  (bit-and 0xefffffff (bit-shift-right x n)))
+
+(defn copy-array
+  "Copy the elements in `from-array` to `to-array`.  Assumes that
+   `to-array` is as long as `from-array`."
+  [^objects from-array ^objects to-array]
+  (loop [c (count from-array)]
+    (when (> c 0)
+      (aset to-array (dec c) (aget from-array (dec c)))
+      (recur (dec c))))
+  to-array)
+
+(defn to-array
+  ([this]
+     (let [o (object-array (-count this))]
+       (loop [curr (-seq this) i 0]
+         (when curr
+           (aset o i (-first curr))
+           (recur (-next curr) (inc i))))
+       o))
+  ([this a]
+     (let [len (-count this)
+          o (if (> len (.length a))
+              (java.lang.reflect.Array/newInstance (-> a .getClass .getComponentType) len)
+              a)]
+      (loop [curr (-seq this) i 0]
+        (when curr
+          (aset o i (-first curr))
+          (recur (-next curr) (inc i))))
+      (when (< len (.lenght a))
+        (aset o len nil))
+      o)))
